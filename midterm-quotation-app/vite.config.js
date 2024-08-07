1bd252abbd07d4904118e418d4dc8797265434f2
Thaw Htut Soe <u6511102@au.edu>
2024-08-07 10:08:51 +0700

    added

--- midterm-quotation-app/vite.config.js
+++ midterm-quotation-app/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'ThawHtutSoe.github.io/midterm-quotation-app/', 
   plugins: [react()]
 });
