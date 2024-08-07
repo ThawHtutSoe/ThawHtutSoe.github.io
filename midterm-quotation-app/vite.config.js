5f93663ab284bb7cf0073be91c0394f8833a3526
Thaw Htut Soe <u6511102@au.edu>
2024-08-07 09:59:20 +0700

    midterm exam

--- midterm-quotation-app/vite.config.js
+++ midterm-quotation-app/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: 'ThawHtutSoe.github.io/midterm-quotation-app/', 
+  plugins: [react()]
+});
